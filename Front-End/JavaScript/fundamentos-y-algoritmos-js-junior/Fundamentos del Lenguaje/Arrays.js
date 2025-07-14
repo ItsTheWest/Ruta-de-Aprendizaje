9b10184f95a544b540c1f2b3036205fa74b8bd5f
ItsTheWest <nelsonjosue0407@gmail.com>
2025-07-14 17:43:15 -0400

    Delete Front-End/JavaScript/fundamentos-y-algoritmos-js-junior directory

--- Front-End/JavaScript/fundamentos-y-algoritmos-js-junior/Fundamentos del Lenguaje/Arrays.js
+++
@@ -1,4 +0,0 @@
-let listanaimales = ["perro", "gato", "loro", "pez", "hamster"];
-console.log(listanaimales);
-listanaimales[0] = "Tortuga";
-console.log(listanaimales);
--- Front-End/JavaScript/fundamentos-y-algoritmos-js-junior/Fundamentos del Lenguaje/objetos.js
+++
@@ -1,9 +0,0 @@
-
-let personaje = {
-    nombre: "Goku",
-    anime: "DragonBall",
-    poder: 90000,
-};
-console.log(personaje);
-delete personaje.anime;
-console.log(personaje);
