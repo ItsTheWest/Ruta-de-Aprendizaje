55d2de170737a85726e7f921d76bb1501fa6f8f3
ItsTheWest <nelsonjosue0407@gmail.com>
2025-07-14 18:13:46 -0400

    Update funciones_integradas.js

--- Front-End/JavaScript/Fundamentos del Lenguaje/funciones_integradas.js
+++ Front-End/JavaScript/Fundamentos del Lenguaje/funciones_integradas.js
@@ -1,8 +1,13 @@
 //funciones integradas en js
 
-//prompt: funcion que permite ingresar un valor por teclado
+//prompt: funcion que permite ingresar un valor por teclado seimpre devuelve un valor string
 let nombre = prompt("Ingrese su nombre");
 console.log(nombre);
+//document write: permite escribir en la vista de la pagina 
+let valor = 1;
+//typeof: permite indicar el tipo de valor de la varible indicada
+document.write("Nombre: " +valor + "</br>")
+document.write("Tipo de valor: " + typeof valor + "<br>");
 
 //alert: funcion que permite mostrar un mensaje en la pantalla
 alert("Hola mundo"); //mensaje en la pantalla
@@ -13,3 +18,4 @@ console.log("Hola mundo");
 
 //document.write: funcion que permite escribir un mensaje directamente en el documento
 document.write("Hola mundo"); 
+
